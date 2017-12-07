36c54a357b8d310fb9fb139229ac0725fdb77468
KevinPark <piaoliwen@outlook.com>
2017-12-07 14:32:21 +0800

    hhgfd

--- ChargingPile/src/main/java/Main.java
+++ ChargingPile/src/main/java/Main.java
@@ -17,8 +17,8 @@ public class Main {
 		System.out.println("charging");
 		System.out.println("charging");
 		System.out.println("charging");	System.out.println("charging");
-
-		System
+fdsa
+		System、
 		syfdshaiodf
 	}
 
