9a26506a9e84a9444cf12a2f18dd2521edd912f7
KevinPark <piaoliwen@outlook.com>
2017-12-07 14:15:12 +0800

    第三份

--- ChargingPile/src/main/java/Main.java
+++ ChargingPile/src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
 		System.out.println("charging");
 		System.out.println("charging");
 		System.out.println("charging");
-		
+		System.out.println("charging");	System.out.println("charging");
 	}
 
 }
